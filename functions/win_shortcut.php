96449091a9455dd8b411704fe97476132f3bfd38
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-09-28 09:02:38 -0400

    Update win_shortcut.php

--- functions/win_shortcut.php
+++ functions/win_shortcut.php
@@ -11,10 +11,5 @@
     <div class="sc_box"><span class="sc_function">Search</span><span class="main_key"> Ctrl </span><i class="fas fa-plus"></i><span class="main_key"> Alt </span><i class="fas fa-plus"></i><span class="sec_key">5</span> </div>
     <div class="sc_box"><span class="sc_function">Spotify</span><span class="main_key"> Ctrl </span><i class="fas fa-plus"></i><span class="main_key"> Alt </span><i class="fas fa-plus"></i><span class="sec_key">6</span> </div>
     <div class="sc_box"><span class="sc_function">Terminal</span><span class="main_key"> Ctrl </span><i class="fas fa-plus"></i><span class="main_key"> Alt </span><i class="fas fa-plus"></i><span class="sec_key">7</span></div>
-    <hr/>
-    <div class="sc_box"><span class="sc_function dexter">Dexter</span><span class="main_key"> Ctrl </span><i class="fas fa-plus"></i><span class="main_key"> Alt </span><i class="fas fa-plus"></i><span class="sec_key">U</span></div>
-    <div class="sc_box"><span class="sc_function chicago">Chicago</span><span class="main_key"> Ctrl </span><i class="fas fa-plus"></i><span class="main_key"> Alt </span><i class="fas fa-plus"></i><span class="sec_key">I</span></div>
-    <div class="sc_box"><span class="sc_function dallas">Dallas</span><span class="main_key"> Ctrl </span><i class="fas fa-plus"></i><span class="main_key"> Alt </span><i class="fas fa-plus"></i><span class="sec_key">O</span></div>
-    <div class="sc_box"><span class="sc_function seattle">Seattle</span><span class="main_key"> Ctrl </span><i class="fas fa-plus"></i><span class="main_key"> Alt </span><i class="fas fa-plus"></i><span class="sec_key">P</span></div>
   </div>
 </div>
