7109bb26370b60fa0ff70ef63882577b3a26ea19
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-05 14:34:30 -0400

    Update win_settings.php

--- functions/win_settings.php
+++ functions/win_settings.php
@@ -8,7 +8,7 @@
   <input type="button" class="set_button" value="Features" onclick="set_feature_toggle();" /></div>
 <div id="set_main">
 <form action="./functions/add_main.php" method="post">
-<div class="section_label">General<input type="submit" name="submit" class="far fa-save" value="&#xf0c7"></div>
+<div class="section_label">General<input type="submit" name="submit" class="far fa-save" value="&#xf0c7" title="Save Changes"></div>
 <div class="set_label">Main URL (no http:// or https://)</div><input type="text" class="set_fields" name="main" value="<?php include './functions/config.php';echo$main ?>">
 <br/>
 <div class="set_label">Page Title</div><input type="text" class="set_fields" name="pagetitle" value="<?php include './functions/config.php';echo$pagetitle ?>">
@@ -21,7 +21,7 @@
 <br/>
 <div class="set_label">Country Code (Lowercase, 2 Charater Max)</div><input type="text" class="set_fields" name="cc" value="<?php include './functions/config.php';echo$cc ?>">
 <hr/>
-<div class="section_label">Feature URLs<input type="submit" name="submit" class="far fa-save" value="&#xf0c7"></div>
+<div class="section_label">Feature URLs<input type="submit" name="submit" class="far fa-save" value="&#xf0c7" title="Save Changes"></div>
 <div class="set_label">Spotify Playlist</div><input type="text" class="set_fields long" name="spot" value="<?php include './functions/config.php';echo$spot ?>">
 <br/>
 <div class="set_label">PrivateBin URL</div><input type="text" class="set_fields long" name="privatebin" value="<?php include './functions/config.php';echo$privatebin ?>">
@@ -37,52 +37,52 @@
 <div id="set_media">
 <div class="section_label">Media</div>
 <div class="wall_box">
-  <div class="wall_head">Morning Wallpaper<span class="current"><a href="./images/background-morning.png" download><i class="fas fa-download"></i></a></span></div>
+  <div class="wall_head">Morning Wallpaper<span class="current"><a title="Download Current Wallpaper" href="./images/background-morning.png" download><i class="fas fa-download"></i></a></span></div>
   <form action="./functions/upload_image_morn.php" method="post" id="uploadmorn" enctype="multipart/form-data">
-      <input type="submit" title="Upload" name="submit" class="fas fa-upload" value="&#xf093" >
+      <input type="submit" title="Upload Wallpaper" name="submit" class="fas fa-upload" value="&#xf093" >
       <input type="file" name="the_file" id="fileToUploadmorn" >
     &nbsp;
   &nbsp;</form></div>
   <hr/>
 <div class="wall_box">
-  <div class="wall_head">Day Wallpaper<span class="current"><a href="./images/background-day.png" download><i class="fas fa-download"></i></a></span></div>
+  <div class="wall_head">Day Wallpaper<span class="current"><a title="Download Current Wallpaper" href="./images/background-day.png" download><i class="fas fa-download"></i></a></span></div>
   <form action="./functions/upload_image_day.php" method="post" id="uploadday" enctype="multipart/form-data">
-      <input type="submit" title="Upload" name="submit" class="fas fa-upload" value="&#xf093" >
+      <input type="submit" title="Upload Wallpaper" name="submit" class="fas fa-upload" value="&#xf093" >
     <input type="file" name="the_file" id="fileToUploadday" >
     &nbsp;
   &nbsp;</form></div>
     <hr/>
 <div class="wall_box">
-  <div class="wall_head">Dusk Wallpaper<span class="current"><a href="./images/background-dusk.png" download><i class="fas fa-download"></i></a></span></div>
+  <div class="wall_head">Dusk Wallpaper<span class="current"><a title="Download Current Wallpaper" href="./images/background-dusk.png" download><i class="fas fa-download"></i></a></span></div>
   <form action="./functions/upload_image_dusk.php" method="post" id="uploaddusk" enctype="multipart/form-data">
-      <input type="submit" title="Upload" name="submit" class="fas fa-upload" value="&#xf093" >
+      <input type="submit" title="Upload Wallpaper" name="submit" class="fas fa-upload" value="&#xf093" >
     <input type="file" name="the_file" id="fileToUploaddusk" >
     &nbsp;
 
   &nbsp;</form></div>
     <hr/>
 <div class="wall_box">
-  <div class="wall_head">Evening Wallpaper<span class="current"><a href="./images/background-night.png" download><i class="fas fa-download"></i></a></span></div>
+  <div class="wall_head">Evening Wallpaper<span class="current"><a title="Download Current Wallpaper" href="./images/background-night.png" download><i class="fas fa-download"></i></a></span></div>
   <form action="./functions/upload_image_evening.php" method="post" id="uploadevening" enctype="multipart/form-data">
-      <input type="submit" title="Upload" name="submit" class="fas fa-upload" value="&#xf093" >
+      <input type="submit" title="Upload Wallpaper" name="submit" class="fas fa-upload" value="&#xf093" >
     <input type="file" name="the_file" id="fileToUploadevening" >
     &nbsp;
 
   &nbsp;</form></div>
     <hr/>
 <div class="wall_box">
-  <div class="wall_head">Mobile Wallpaper<span class="current"><a href="./images/background-mobile.png" download><i class="fas fa-download"></i></a></span></div>
+  <div class="wall_head">Mobile Wallpaper<span class="current"><a title="Download Current Wallpaper" href="./images/background-mobile.png" download><i class="fas fa-download"></i></a></span></div>
   <form action="./functions/upload_image_mobile.php" method="post" id="uploadmobile" enctype="multipart/form-data">
-      <input type="submit" title="Upload" name="submit" class="fas fa-upload" value="&#xf093" >
+      <input type="submit" title="Upload Wallpaper" name="submit" class="fas fa-upload" value="&#xf093" >
     <input type="file" name="the_file" id="fileToUploadmobile" >
     &nbsp;
 
   &nbsp;</form></div>
   <hr/>
 <div class="wall_box">
-  <div class="wall_head">Logo<span class="current"><a href="./images/logo.png" download><i class="fas fa-download"></i></a></span></div>
+  <div class="wall_head">Logo<span class="current"><a title="Download Current Wallpaper" href="./images/logo.png" download><i class="fas fa-download"></i></a></span></div>
   <form action="./functions/upload_image_logo.php" method="post" id="uploadlogo" enctype="multipart/form-data">
-      <input type="submit" title="Upload" name="submit" class="fas fa-upload" value="&#xf093" >
+      <input type="submit" title="Upload Wallpaper" name="submit" class="fas fa-upload" value="&#xf093" >
     <input type="file" name="the_file" id="fileToUploadlogo"  >
     &nbsp;
 
@@ -92,7 +92,7 @@
 
 <div id="set_messages">
 <form action="./functions/add_media.php" method="post" id="media_form"></form>
-<div class="section_label">Messages<input type="submit" name="submit" class="far fa-save" value="&#xf0c7"></div>
+<div class="section_label">Messages<input type="submit" name="submit" class="far fa-save" value="&#xf0c7" title="Save Changes"></div>
 <div class="set_label">Morning 5am-10am</div>
 <input type="text" class="set_fields long" form="media_form" name="morning" value="<?php include './functions/config.php';echo"$morning" ?>">
   <hr/>
