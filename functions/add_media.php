2b6ae9a5a2305a07e4f66c06308070979b460dc6
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-05 15:53:27 -0400

    Update add_media.php

--- functions/add_media.php
+++ functions/add_media.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML >
 <html lang="en">
 <head>
-<?php 
+<?php
 include 'config.php';
 echo "<meta http-equiv='refresh' content='2; url=https://$main/' />";
 ?>
@@ -12,19 +12,19 @@ echo "<meta http-equiv='refresh' content='2; url=https://$main/' />";
 <body class="quickpage">
 <?php
 include 'source_db.php';
-    $conn = mysqli_connect("localhost", $username, $password, $dbname); 
+    $conn = mysqli_connect("localhost", $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 if(isset($_POST['submit']))
-{    
+{
         $id='1';
         $morning = $_POST['morning'];
         $day = $_POST['day'];
         $dusk = $_POST['dusk'];
-        $evening = $_POST['evening']; 
-        $font = $_POST['font']; 
-        
+        $evening = $_POST['evening'];
+        $font = $_POST['font'];
+
         $morning = trim($morning);
         $day = trim($day);
         $dusk = trim($dusk);
@@ -32,9 +32,9 @@ if(isset($_POST['submit']))
         $font = trim($font);
 
      $sql = "UPDATE settings SET morning_message='$morning' , day_message='$day' , dusk_message='$dusk' , evening_message='$evening', font='$font' WHERE id='$id' ";
-} 
+}
 if (mysqli_query($conn, $sql)) {
-    echo "Media Settings Updated";
+    echo "Message Settings Updated";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
