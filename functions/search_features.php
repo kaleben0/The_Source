c9093529233856fbd7f3c0e6cad78eeb3ede2785
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-08 09:41:24 -0400

    Update search_features.php

--- functions/search_features.php
+++ functions/search_features.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
         include 'source_db.php';
         $mysqli = new mysqli( "localhost", $username, $password, $dbname );
         $query1 = "SELECT * FROM features WHERE type = 'search' ORDER BY title ";
@@ -19,7 +19,7 @@
 	            $color = 'red';
 	            $change = './functions/add_feat.php';
 	            }
-        echo "<div class='feat_container'><div class='feat_title'>$title</div><div class='feat_url'><a href='$change?id=$id&title=$title&status=$status'><i class='$sub_icon' style='color:$color !important;'></i></a></div></div>";
+        echo "<div class='feat_container'><div class='feat_title'>$title</div><div class='feat_url'><a href='$change?id=$id&title=$title&status=$status' target='_blank'><i class='$sub_icon' style='color:$color !important;'></i></a></div></div>";
           }
           $result->free();
         }
