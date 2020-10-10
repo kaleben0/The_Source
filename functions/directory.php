033f00dd391ba854a054a37c47101ee4d2a912a3
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-10 16:40:58 -0400

    Update directory.php

--- functions/directory.php
+++ functions/directory.php
@@ -1,17 +1,12 @@
-<div class="container" id="directory_box"> <br/>
-  <br/>
-  <div class="row">
-    <div class="category_block"  id="directory_section">
-      <div class="directory_block">
-        <div class="directory_bar">
-          <i class="far fa-edit fa-1x mybutton editor" onclick="edit_book();" title="Update All Bookmarks" id="bookmark_update"></i>
-          <i class="far fa-bookmark fa-1x mybutton editor" onclick="toggle_book();" title="Add Bookmark" id="bookmark_add"></i>
-          <i class="far fa-trash-alt fa-1x mybutton editor" onclick="delete_edit();" title="Delete" id="bookmark_delete"></i>
-          <i class="far fa-star fa-1x mybutton editor" onclick="favorite_edit();" title="Favorite" id="bookmark_favorite"></i>
+<div class="category_block"  id="directory_section">
+<div class="directory_bar">
+<i class="far fa-edit fa-1x mybutton editor" onclick="edit_book();" title="Update All Bookmarks" id="bookmark_update"></i>
+<i class="far fa-bookmark fa-1x mybutton editor" onclick="toggle_book();" title="Add Bookmark" id="bookmark_add"></i>
+<i class="far fa-trash-alt fa-1x mybutton editor" onclick="delete_edit();" title="Delete" id="bookmark_delete"></i>
+<i class="far fa-star fa-1x mybutton editor" onclick="favorite_edit();" title="Favorite" id="bookmark_favorite"></i>
+</div>
+<br/>
 <?php
-include './functions/directory_box.php';
+include 'directory_box.php';
 ?>
-      </div>
-    </div>
-  </div>
 </div>
