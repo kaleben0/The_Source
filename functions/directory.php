18a6f262c881b9d8981ab17ab820d246ed71344f
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-10 14:18:07 -0400

    Update directory.php

--- functions/directory.php
+++ functions/directory.php
@@ -3,7 +3,11 @@
   <div class="row">
     <div class="category_block"  id="directory_section">
       <div class="directory_block">
-        <div class="directory_bar"><i class="far fa-edit fa-1x mybutton editor" onclick="edit_book();" title="Update All Bookmarks" id="bookmark_update"></i><i class="far fa-bookmark fa-1x mybutton editor" onclick="toggle_book();" title="Add Bookmark" id="bookmark_add"></i>  <i class="far fa-star fa-1x mybutton editor" onclick="favorite_edit();" title="Favorite" id="bookmark_favorite"></i> <i class="far fa-trash-alt fa-1x mybutton editor" onclick="delete_edit();" title="Delete" id="bookmark_delete"></i> </div>
+        <div class="directory_bar">
+          <i class="far fa-edit fa-1x mybutton editor" onclick="edit_book();" title="Update All Bookmarks" id="bookmark_update"></i>
+          <i class="far fa-bookmark fa-1x mybutton editor" onclick="toggle_book();" title="Add Bookmark" id="bookmark_add"></i>
+          <i class="far fa-trash-alt fa-1x mybutton editor" onclick="delete_edit();" title="Delete" id="bookmark_delete"></i>
+          <i class="far fa-star fa-1x mybutton editor" onclick="favorite_edit();" title="Favorite" id="bookmark_favorite"></i>
 <?php
 include './functions/directory_box.php';
 ?>
