d1cbc2046fe627fbfa55d431a6c7d0858daf0cb2
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-10 16:54:51 -0400

    Update letter_bar.php

--- functions/letter_bar.php
+++ functions/letter_bar.php
@@ -2,32 +2,32 @@
    <div id="letter_row">
       <div class="letter_center">
      <div class="letters" >
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#a">A</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#b">B</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#c">C</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#d">D</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#e">E</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#f">F</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#g">G</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#h">H</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#i">I</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#j">J</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#k">K</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#l">L</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#m">M</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#n">N</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#o">O</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#p">P</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#q">Q</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#r">R</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#s">S</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#t">T</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#u">U</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#v">V</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#w">W</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#x">X</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#y">Y</a> </div>
-        <div class="letter_space"><a class="js-scroll-trigger letter_btn" onclick="directory_toggle();" href="#z">Z</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#a">A</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#b">B</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#c">C</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#d">D</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#e">E</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#f">F</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#g">G</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#h">H</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#i">I</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#j">J</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#k">K</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#l">L</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#m">M</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#n">N</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#o">O</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#p">P</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#q">Q</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#r">R</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#s">S</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#t">T</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#u">U</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#v">V</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#w">W</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#x">X</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#y">Y</a> </div>
+        <div class="letter_space"><a class="letter_btn" onclick="directory_toggle();" href="#z">Z</a> </div>
       </div>
     </div>
   </div>
