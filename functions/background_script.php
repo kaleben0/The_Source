7fe352ef7d99483e5c46c557034954466a23ca8e
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-14 15:11:50 -0400

    Update background_script.php

--- functions/background_script.php
+++ functions/background_script.php
@@ -5,27 +5,17 @@ $(document).ready(function () {
   if (n > 4 && n < 10)
     $("body").css("background-image", "url('../images/background-morning.png')"),
     $('#time_message').text("<?php include './functions/config.php';echo $morning ?>");
-        });
-    });
   else if (n > 16 && n < 22)
     $("body").css("background-image", "url('../images/background-dusk.png')"),
     $('#time_message').text("<?php include './functions/config.php';echo $dusk ?>");
-        });
-    });
   else if (n > 21 && n < 25)
     $("body").css("background-image", "url('../images/background-night.png')"),
     $('#time_message').text("<?php include './functions/config.php';echo $evening ?>");
-        });
-    });
   else if (n > 0 && n < 5)
     $("body").css("background-image", "url('../images/background-night.png')"),
     $('#time_message').text("<?php include './functions/config.php';echo $evening ?>");
-        });
-    });
   else
     $("body").css("background-image", "url('../images/background-day.png')"),
     $('#time_message').text("<?php include './functions/config.php';echo $day ?>");
-        });
-    });
-});
+ });
 </script>
