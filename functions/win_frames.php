c545cd8537cf6812a3934819a1d31440dcbecb68
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-15 17:12:28 -0400

    Update win_frames.php

--- functions/win_frames.php
+++ functions/win_frames.php
@@ -1,29 +1,38 @@
-<div class="win_frame1"><span class="exit_frame1" ><i class="fas fa-window-close fa-lg"></i></span>
-  <div class="frame_head"><?php include './functions/config_frames.php';echo$frame1_title ?></div>
-</div>
-<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame1_url ?>" width="<?php include './functions/config_frames.php';echo$frame1_width ?>" height="<?php include './functions/config_frames.php';echo$frame1_height ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-</div>
-
-<div class="win_frame2"><span class="exit_frame2" ><i class="fas fa-window-close fa-lg"></i></span>
-  <div class="frame_head"><?php include './functions/config_frames.php';echo$frame1_title ?></div>
-</div>
-<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame2_url ?>" width="<?php include './functions/config_frames.php';echo$frame2_width ?>" height="<?php include './functions/config_frames.php';echo$frame2_height ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+<div class="win_frame1" style="width:<?php include './functions/config_frames.php';echo$frame1_width?>px;height:<?php include './functions/config_frames.php';echo$frame1_height ?>px;"><span class="exit_frame1" ><i class="fas fa-window-close fa-lg"></i></span>
+  <div class="frame_head">
+    <?php include './functions/config_frames.php';echo$frame1_title ?>
+  </div>
+<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame1_url ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 
-<div class="win_frame3"><span class="exit_frame3" ><i class="fas fa-window-close fa-lg"></i></span>
-  <div class="frame_head"><?php include './functions/config_frames.php';echo$frame3_title ?></div>
-</div>
-<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame3_url ?>" width="<?php include './functions/config_frames.php';echo$frame3_width ?>" height="<?php include './functions/config_frames.php';echo$frame3_height ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<div class="win_frame2" style="width:<?php include './functions/config_frames.php';echo$frame2_width?>px;height:<?php include './functions/config_frames.php';echo$frame2_height ?>px;"><span class="exit_frame2" ><i class="fas fa-window-close fa-lg"></i></span>
+  <div class="frame_head">
+    <?php include './functions/config_frames.php';echo$frame2_title ?>
+  </div>
+<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame2_url ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 
-<div class="win_frame4"><span class="exit_frame4" ><i class="fas fa-window-close fa-lg"></i></span>
-  <div class="frame_head"><?php include './functions/config_frames.php';echo$frame4_title ?></div>
-</div>
-<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame4_url ?>" width="<?php include './functions/config_frames.php';echo$frame4_width ?>" height="<?php include './functions/config_frames.php';echo$frame4_height ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+<div class="win_frame3" style="width:<?php include './functions/config_frames.php';echo$frame3_width?>px;height:<?php include './functions/config_frames.php';echo$frame3_height ?>px;">
+<span class="exit_frame3"><i class="fas fa-window-close fa-lg"></i></span>
+  <div class="frame_head">
+    <?php include './functions/config_frames.php';echo$frame3_title ?>
+  </div>
+<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame3_url ?>"  frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
 
-<div class="win_frame5"><span class="exit_frame5" ><i class="fas fa-window-close fa-lg"></i></span>
-  <div class="frame_head"><?php include './functions/config_frames.php';echo$frame1_title ?></div>
+<div class="win_frame4" style="width:<?php include './functions/config_frames.php';echo$frame4_width?>px;height:<?php include './functions/config_frames.php';echo$frame4_height ?>px;"><span class="exit_frame4" ><i class="fas fa-window-close fa-lg"></i></span>
+  <div class="frame_head">
+    <?php include './functions/config_frames.php';echo$frame4_title ?>
+  </div>
+<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame4_url ?>"  frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
-<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame5_url ?>" width="<?php include './functions/config_frames.php';echo$frame5_width ?>" height="<?php include './functions/config_frames.php';echo$frame5_height ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+
+<div class="win_frame5" style="width:<?php include './functions/config_frames.php';echo$frame5_width?>px;height:<?php include './functions/config_frames.php';echo$frame5_height ?>px;"><span class="exit_frame5" ><i class="fas fa-window-close fa-lg"></i></span>
+  <div class="frame_head">
+    <?php include './functions/config_frames.php';echo$frame5_title ?>
+  </div>
+<iframe class="frame" src="<?php include './functions/config_frames.php';echo$frame5_url ?>" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 </div>
