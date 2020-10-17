61a427d01f1ef85c391175731daccd59919a50a3
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-17 12:40:39 -0400

    formatting help section

--- functions/win_help.php
+++ functions/win_help.php
@@ -5,17 +5,20 @@
   <div class="help_box">
     <Div class="help_img"><a href="./images/help1.png" target="_blank"><img src="./images/help1.png" width="300px" height="200px" /></a></div>
     <h2>Overview</h2>
-    The Source is a customizable start page that provides quick access to tools, search engines and important websites. <br/>
+    <p>The Source is a customizable start page that provides quick access to tools, search engines and important websites.</p>
+    </p>
     <h3><i class="far fa-bookmark"></i> Bookmarks</h3>
-    Bookmarks can be managed by accessing the main directory or a category. <br/>
-    The main directory can be accessed by clicking a letter on the letter bar. <br/>
+    <p>Bookmarks can be managed by accessing the main directory or a category.</p>
+    <p>The main directory can be accessed by clicking a letter on the letter bar.</p>
+    </p>
     <h3><i class="far fa-star"></i> Favorite Section</h3>
-    Favorites are bookmarks that have been selected in the directory to display in the Favorite section. <br/>
-    The Favorite Section is the first area displayed on page load. <br/>
+    <p>Favorites are bookmarks that have been selected in the directory to display in the Favorite section.</p>
+    The Favorite Section is the first area displayed on page load.
+    </p>
     <h3><i class="fas fa-th-list"></i> Category Section</h3>
-    Bookmarks here are sorted by one of 9 different categories. <br/>
-    Categories can be hidden in the Settings Menu. <br/>
-    Categories are :
+    <p>Bookmarks here are sorted by one of 9 different categories. </p>
+    <p>Categories can be hidden in the Settings Menu.</p>
+    <p>Categories are :</p>
     <ul>
       <li>Design</li>
       <li>Environmental</li>
@@ -29,63 +32,64 @@
     </ul>
     <h2><i class="fas fa-tools"></i> Features</h2>
     <h3><i class="fab fa-google"></i> Google Search</h3>
-    The Google Search bar is located on the Top Navigation Bar. <br/>
-    Queries are sent to Google's search engine. <br/>
-    The spyglass icon on the left of the bar will clear the input box. <br/>
-    The spyglass on the right of the bar will launch the Multi-Search window. <br/>
+    <p>The Google Search bar is located on the Top Navigation Bar. </p>
+    <p>Queries are sent to Google's search engine.</p>
+    <p>The spyglass icon on the left of the bar will clear the input box.</p>
+    <p>The spyglass on the right of the bar will launch the Multi-Search window.</p>
     <h3><i class="fas fa-search-plus"></i> Multi-Search</h3>
-    The Multi-Search window allows the user to search multiple sites from one interface. <br/>
-    Queries are sent to search engine button that is click. <br/>
-    The topmost part of the window contains pre-defined searches for various Google services. <br/>
-    The bottom most part contains custom searches that can be configured in the settings menu. <br/>
+    <p>The Multi-Search window allows the user to search multiple sites from one interface.</p>
+    <p>Queries are sent to search engine button that is click.</p>
+    <p>The topmost part of the window contains pre-defined searches for various Google services.</p>
+    <p>The bottom most part contains custom searches that can be configured in the settings menu. </p>
     <h3><i class="fas fa-cloud-sun"></i> Weather</h3>
-    The Weather Window will display a customizable weather forecast. <br/>
-    The widget can be customized in the settings menu. <br/>
+    <p>The Weather Window will display a customizable weather forecast.</p>
+    <p>The widget can be customized in the settings menu.</p>
     <h3><i class="fas fa-unlock-alt"></i> Password Generator</h3>
-    This window will generate a random password. <br/>
-    The user specifies the length. <br/>
-    Characters available: abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+-=?@_ <br/>
+    <p>This window will generate a random password.</p>
+    <p>The user specifies the length.</p>
+    <p>Characters available: </p>
+    <p>abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+-=?@_ </p>
     <h3><i class="fas fa-envelope-open-text"></i> Custom Messages</h3>
-    The Custom Messages is displayed on the Top Navigation Bar. <br/>
-    These messages can be configured in the Settings Menu. <br/>
+    <p>The Custom Messages is displayed on the Top Navigation Bar. </p>
+    <p>These messages can be configured in the Settings Menu.</p>
     <h3><i class="far fa-file-image"></i> Wallpaper</h3>
-    Wallpaper can be customized in the Settings Menu. <br/>
+    <p>Wallpaper can be customized in the Settings Menu.</p>
     <h3><i class="fas fa-window-restore"></i> Frames</h3>
-    The Source will display up to 5 customizable iframes. <br/>
-    Content and dimensions of the iframes can be set up in the Settings Menu <br/>
+    <p>The Source will display up to 5 customizable iframes.</p>
+    <p>Content and dimensions of the iframes can be set up in the Settings Menu</p>
     <h2><i class="fas fa-ellipsis-h"></i> Navigation</h2>
     <h3><i class="fas fa-list"></i> Main Directory</h3>
-    The Main Directory will display all bookmarks. <br/>
-    It can be accessed by clicking on one of the letters on the Letter Bar. <br/>
-    A control bar will appear at the top of the screen. <br/>
-    This bar will also appear when accessing a category. <br/>
+    <p>The Main Directory will display all bookmarks. </p>
+    <p>It can be accessed by clicking on one of the letters on the Letter Bar.</p>
+    <p>A control bar will appear at the top of the screen. </p>
+    <p>This bar will also appear when accessing a category.</p>
     <h4><i class="far fa-star"></i> Favorite</h4>
-    Toggles the "favorite" icon to be displayed. Allows the selection of favorite bookmarks.
+    <p>Toggles the "favorite" icon to be displayed. Allows the selection of favorite bookmarks.</p>
     <h4><i class="far fa-trash-alt"></i> Remove</h4>
-    Toggles the "Remove" icon to be displayed. Allows the removal of bookmarks. <br/>
-    **Warning: Once removed, you will need to re-add the bookmark via the Add Bookmark window.
+    <p>Toggles the "Remove" icon to be displayed. Allows the removal of bookmarks.</p>
+    <p><b>Warning: Once removed, you will need to re-add the bookmark via the Add Bookmark window.</b></p>
     <h4><i class="far fa-bookmark"></i> Add Bookmark</h4>
-    Add a single bookmark to your collection
+    <p>Add a single bookmark to your collection</p>
     <h4><i class="far fa-edit"></i> Edit Bookmarks</h4>
-    Opens the MTE interface to Add/Remove/Change multiple bookmarks.
+    <p>Opens the MTE interface to Add/Remove/Change multiple bookmarks.</p>
     <h4><i class="fas fa-list"></i> List View</h4>
-    Toggles between Grid / List view
+    <p>Toggles between Grid / List view</p>
     <h3><i class="fas fa-ellipsis-v"></i> Side Menu</h3>
-    The Side Menu is a quick menu that allows access to Favorites, Categories, Password Generator, Multi-Search, and your Custom Frames.
+    <p>The Side Menu is a quick menu that allows access to Favorites, Categories, Password Generator, Multi-Search, and your Custom Frames.</p>
     <h3><i class="fas fa-reply fa-flip-horizontal"></i> Keyboard Shortcuts</h3>
-    Displays the list of available Keyboard Shortcuts. <br/>
+    <p>Displays the list of available Keyboard Shortcuts.</p>
     <h3><i class="fas fa-user-cog" ></i> User Management</h3>
-    Opens the HTAM (HTAccess Manager). <br/>
-    Users and security settings can be managed here. <br/>
+    <p>Opens the HTAM (HTAccess Manager). </p>
+    <p>Users and security settings can be managed here.</p>
     <h2><i class="fas fa-cogs"></i> Settings</h2>
     <h3><i class="fas fa-vector-square"></i> General</h3>
     <h4><i class="fas fa-atlas"></i> Main URL</h4>
-    Add your domain here (without the https:// or http://). <br/>
-    This is used in different areas of the page <br/>
+    <p>Add your domain here (without the https:// or http://).</p>
+    <p>This is used in different areas of the page
     <h4><i class="far fa-file-alt"></i> Page Title</h4>
-    Displays in the tab of the page
+    <p>Displays in the tab of the page</p>
     <h4><i class="fas fa-font"></i> Font Selection</h4>
-    Select one of 5 different Google fonts for the page.
+    <p>Select one of 5 different Google fonts for the page.</p>
     <ul>
       <li>Comic Neue</li>
       <li>Open Sans</li>
@@ -94,16 +98,16 @@
       <li>Source Sans Pro</li>
     </ul>
     <h3><i class="far fa-file-image"></i> Weather</h3>
-    After configuring a 110px width weather widget from https://weatherwidget.io/ , paste the code here to update the Weather Window
+    <p>After configuring a 110px width weather widget from https://weatherwidget.io/ , paste the code here to update the Weather Window</p>
     <h3><i class="fas fa-envelope-open-text"></i> Messages</h3>
-    The message that appears in the Top Navigation Bar can be customized here. <br/>
-    The message that is displayed is determined by the time of day. <br/>
+    <p>The message that appears in the Top Navigation Bar can be customized here.</p>
+    <p>The message that is displayed is determined by the time of day.</p>
     <h3><i class="fas fa-photo-video"></i> Media</h3>
-    Wallpaper, Mobile Wallpaper, and Logo can be customized in the Media section of the Settings Menu.
+    <p>Wallpaper, Mobile Wallpaper, and Logo can be customized in the Media section of the Settings Menu.</p>
     <h3><i class="fas fa-window-restore"></i> Custom Frames</h3>
-    In the Custom Frames section of the Settings Menu, you can customize 5 different iframes. <br/>
-    * Note: Some websites have restrictions that prevent their pages from displaying in an iframe. <br/>
-    Customize Options :
+    <p>In the Custom Frames section of the Settings Menu, you can customize 5 different iframes.</p>
+    <p><b>Note: Some websites have restrictions that prevent their pages from displaying in an iframe.<b/></p>
+    <p>Customize Options :</p>
     <ul>
       <li>Title</li>
       <li>URL</li>
@@ -111,21 +115,21 @@
       <li>Window Dimensions</li>
     </ul>
     <h3><i class="fas fa-search-plus"></i> Custom Search</h3>
-    In the Custom Search section of the Settings Menu, you can configure up to 24 different searches that will appear in the Multi-Search Window. <br/>
-    * Note: To disable a search, clear the fields and type empty into the title. <br/>
-    Customize Options :
+    <p>In the Custom Search section of the Settings Menu, you can configure up to 24 different searches that will appear in the Multi-Search Window.</p>
+    <p><b>Note: To disable a search, clear the fields and type empty into the title.</b></p>
+    <p>Customize Options :</p>
     <ul>
       <li>Title</li>
       <li>URL</li>
       <li>Font Awesome Icon</li>
     </ul>
     <h3><i class="fas fa-th-list"></i> Categories</h3>
-    In the Categories section of the Settings Menu, unused categories can be enabled/disabled
+    <p>In the Categories section of the Settings Menu, unused categories can be enabled/disabled</p>
     <h3><i class="fas fa-tools"></i> Features</h3>
-    In the Features section of the Settings Menu, unused features can be enabled/disabled
+    <p>In the Features section of the Settings Menu, unused features can be enabled/disabled</p>
     <h3><i class="fab fa-google"></i> Google Search</h3>
-    In the Google Searches section of the Settings Menu, unused Google Searches can be enabled/disabled.
-    Customize Options :
+    <p>In the Google Searches section of the Settings Menu, unused Google Searches can be enabled/disabled.</p>
+    <p>Customize Options :</p>
     <ul>
       <li>Google Calendar</li>
       <li>Google Contacts</li>
@@ -142,12 +146,11 @@
       <li>YouTube</li>
     </ul>
     <h3><i class="fab fa-css3-alt"></i> Custom CSS</h3>
-    In the Custom CSS section of the Settings Menu, CSS for The Source can be overwritten with your own CSS. <br/>
-    <br/>
+    <p>In the Custom CSS section of the Settings Menu, CSS for The Source can be overwritten with your own CSS.</p>
     <h3><i class="fas fa-info-circle"></i> Credits</h3>
     <h4>The Source v1.0</h4>
-    Developed by Doug Redman<br/>
-    GitHub : https://github.com/kaleben0/The_Source<br/>
+    <p>Developed by Doug Redman</p>
+    <p>GitHub : https://github.com/kaleben0/The_Source</p>
     <h5>Software/Plugins Utilized</h5>
     <ul>
       <li>Tipr - https://github.com/lt1946/Tipr</li>
@@ -159,6 +162,7 @@
       <li>Font Awesome - https://fontawesome.com/</li>
       <li>Weather Widget - https://weatherwidget.io/</li>
     </ul>
-    This is completely open source and you can do anything you want with it. <br/>
-    Some of the software and plugins used to make it have their own licenses, but everything I did is open game. </div>
+    <p>This is completely open source and you can do anything you want with it.</p>
+    <p>Some of the software and plugins used to make it have their own licenses, but everything I did is open game.</p>
+  </div>
 </div>
