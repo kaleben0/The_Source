721e6d902db05720f4ff6db231fca9c89aca4375
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-18 15:43:53 -0400

    Update settings_messages.php

--- functions/settings_messages.php
+++ functions/settings_messages.php
@@ -5,12 +5,12 @@
   </div>
   <div class="settings_box" id="set_mess">
     <div class="set_label">Morning 5am-10am</div>
-    <input type="text" class="set_fields long" form="media_form" name="morning" value="<?php include './functions/config.php';echo"$morning" ?>" maxlength="15">
+    <input type="text" class="set_fields long" form="media_form" name="morning" value="<?php include './functions/config.php';echo"$morning" ?>" maxlength="16">
     <div class="set_label">Day 10am-5pm</div>
-    <input type="text" class="set_fields long" form="media_form" name="day" value="<?php include './functions/config.php';echo"$day" ?>" maxlength="15">
+    <input type="text" class="set_fields long" form="media_form" name="day" value="<?php include './functions/config.php';echo"$day" ?>" maxlength="16">
     <div class="set_label">Dusk 5pm-10pm</div>
-    <input type="text" class="set_fields long" form="media_form" name="dusk" value="<?php include './functions/config.php';echo"$dusk"?>" maxlength="15">
+    <input type="text" class="set_fields long" form="media_form" name="dusk" value="<?php include './functions/config.php';echo"$dusk"?>" maxlength="16">
     <div class="set_label">Evening 10pm-5am</div>
-    <input type="text" class="set_fields long" form="media_form" name="evening" value="<?php include './functions/config.php';echo"$evening" ?>" maxlength="15">
+    <input type="text" class="set_fields long" form="media_form" name="evening" value="<?php include './functions/config.php';echo"$evening" ?>" maxlength="16">
   </div>
 </form>
