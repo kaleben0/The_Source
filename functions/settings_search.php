8b707dc71a192924526810f61ad1a8a74328b71f
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-18 15:44:18 -0400

    Update settings_search.php

--- functions/settings_search.php
+++ functions/settings_search.php
@@ -14,7 +14,7 @@
 echo "<form action='./functions/update_search.php' method='post' id='search_form$id'>";
 echo "<div class='set_label'>Search $id  <div class='tooltip'><i class='fas fa-info-circle fa-sm'></i><span class='tooltiptext'>Change Title to 'empty' (without quotes) to disable</span></div><input type='submit' name='submit' class='far fa-save' value='&#xf0c7' title='Save Changes'></div>";
 echo "<input type='hidden' name='id' value='$id'>";
-echo "<div class='frame_field'>Title <input type='text' class='set_fields' name='search_title' maxlength='13' value='$search_title'></div>";
+echo "<div class='frame_field'>Title <input type='text' class='set_fields' name='search_title' maxlength='14' value='$search_title'></div>";
 echo "<div class='frame_field'>URL <input type='text' class='set_fields url' name='search_url' value='$search_url' ></div>";
 echo "<div class='frame_field'>Font Awesome Icon <input type='text' class='set_fields frame_icon' name='search_icon' value='$search_icon'></div>";
 echo "</form></br>";
