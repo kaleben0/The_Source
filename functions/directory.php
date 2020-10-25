7d468b589b3558c0a2ccbe3d9db2218754a936ec
Kaleben0 <52037180+kaleben0@users.noreply.github.com>
2020-10-25 17:26:03 -0400

    Update directory.php

--- functions/directory.php
+++ functions/directory.php
@@ -1,7 +1,7 @@
 <div class="category_block"  id="directory_section">
 <div class="directory_bar">
 <i class="fas fa-list fa-1x mybutton editor" title="List View" id="list_toggle"></i>
-<i class="far fa-edit fa-1x mybutton editor" onclick="edit_book();" title="Edit All Bookmarks" id="bookmark_update"></i>
+<i class="far fa-edit fa-1x mybutton editor" onclick="toggle_edit_book();" title="Edit Bookmarks" id="bookmark_update"></i>
 <i class="far fa-bookmark fa-1x mybutton editor" onclick="toggle_book();" title="Add Bookmark" id="bookmark_add"></i>
 <i class="far fa-trash-alt fa-1x mybutton editor" onclick="delete_edit();" title="Delete" id="bookmark_delete"></i>
 <i class="far fa-star fa-1x mybutton editor" onclick="favorite_edit();" title="Favorite" id="bookmark_favorite"></i>
